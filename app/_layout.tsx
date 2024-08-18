169ccb5965eaa6760b7523d7026d5eb37a907a7a
Emilio H. Correa <ehcorrea60@gmail.com>
2024-08-18 16:17:19 -0300

    chore: adiciona propriedades do theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,27 @@
-import { Stack } from "expo-router";
+import { useEffect } from 'react';
+import { Stack, SplashScreen } from 'expo-router';
+import { useFonts } from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    'SofiaPro-Light': require('@/assets/fonts/SofiaPro-Light.ttf'),
+    'SofiaPro-Regular': require('@/assets/fonts/SofiaPro-Regular.ttf'),
+    'SofiaPro-Medium': require('@/assets/fonts/SofiaPro-Medium.ttf'),
+    'SofiaPro-SemiBold': require('@/assets/fonts/SofiaPro-SemiBold.ttf'),
+    'SofiaPro-Bold': require('@/assets/fonts/SofiaPro-Bold.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
   return (
     <Stack>
       <Stack.Screen name="index" />
--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,6 +1,49 @@
 import { rfvalue, rhvalue, rwvalue } from '@/utils/responsive/responsive';
 
 export const theme = {
+  font: {
+    weight: {
+      light: 'SofiaPro-Light',
+      regular: 'SofiaPro-Regular',
+      medium: 'SofiaPro-Medium',
+      semi: 'SofiaPro-SemiBold',
+      bold: 'SofiaPro-Bold',
+    },
+    size: {
+      small: rfvalue(12),
+      medium: rfvalue(15),
+      large: rfvalue(18),
+      xlarge: rfvalue(24),
+      huge: rfvalue(30),
+      xhuge: rfvalue(26),
+    },
+  },
+  colors: {
+    primary: {
+      main: '#FE724C',
+      80: '#FE8160',
+      50: '#FEA58D',
+      20: '#FED2C7',
+    },
+    secondary: {
+      main: '#FFC529',
+      80: '#FFD050',
+      50: '#FFDF8B',
+      20: '#FFEFC3',
+    },
+    black: {
+      main: '#1A1D26',
+      80: '#2A2F3D',
+      50: '#4D5364',
+      20: '#6E7489',
+    },
+    gray: {
+      main: '#9A9FAE',
+      80: '#A8ACB9',
+      50: '#C4C7D0',
+      20: '#EBEBEB',
+    },
+  },
   rfvalue,
   rwvalue,
   rhvalue,

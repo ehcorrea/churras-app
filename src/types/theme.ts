4cc39b3dd4d7b9a0300c8027ed10cfb5f7e4ecae
Emilio H. Correa <ehcorrea60@gmail.com>
2024-08-20 15:58:30 -0300

    chore: add types com base em theme

---
+++ src/types/theme.ts
@@ -0,0 +1,5 @@
+import { theme } from '@/constants';
+
+export type Color = keyof typeof theme.colors;
+export type FontSize = keyof typeof theme.font.size;
+export type FontWight = keyof typeof theme.font.weight.light;

43b500bd8691ac6e8491fe932272a82aef487df8
Emilio H. Correa <ehcorrea60@gmail.com>
2024-08-24 12:03:53 -0300

    feat: add metodo para setar header nas requisicoes

--- src/services/client.ts
+++ src/services/client.ts
@@ -4,4 +4,8 @@ const defaultOptions: AxiosRequestConfig = {
   baseURL: process.env.EXPO_PUBLIC_API_URL,
 };
 
+export const setHeaderAuthorization = (jwt: string) => {
+  client.defaults.headers.common.Authorization = `bearer ${jwt}`;
+};
+
 export const client = axios.create(defaultOptions);

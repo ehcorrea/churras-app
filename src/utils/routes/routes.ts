ce0ef3d816a9e0c6d4b65f766eb74f3f3b384c32
Emilio H. Correa <ehcorrea60@gmail.com>
2024-08-25 22:08:18 -0300

    feat: add utils para rotas

---
+++ src/utils/routes/routes.ts
@@ -0,0 +1,16 @@
+type TabBarRoutes = Record<string, { title: string; icon: string }>;
+
+export const tabBarRoutes: TabBarRoutes = {
+  home: {
+    title: 'Home',
+    icon: 'home',
+  },
+  eventos: {
+    title: 'Eventos',
+    icon: 'calendar',
+  },
+  menu: {
+    title: 'Menu',
+    icon: 'user',
+  },
+};

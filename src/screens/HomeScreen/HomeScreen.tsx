f05208791812df47abbe6a076f6084774fab6165
Emilio H. Correa <ehcorrea60@gmail.com>
2024-08-27 13:30:59 -0300

    chore: add bannerad e calendar para home

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,11 +1,31 @@
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useRef } from 'react';
+import { Platform, View } from 'react-native';
+import {
+  BannerAd,
+  BannerAdSize,
+  TestIds,
+  useForeground,
+} from 'react-native-google-mobile-ads';
 
-import { Text } from '@/components';
+import { Calendar, HeaderUser, Spancing, Text } from '@/components';
 
 export function HomeScreen() {
+  const bannerRef = useRef<BannerAd>(null);
+  useForeground(() => {
+    Platform.OS === 'ios' && bannerRef.current?.load();
+  });
+
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
-    </SafeAreaView>
+    <View style={{ backgroundColor: '#FFFFFFFF', flex: 1 }}>
+      <HeaderUser />
+      <BannerAd
+        ref={bannerRef}
+        size={BannerAdSize.FULL_BANNER}
+        unitId={TestIds.ADAPTIVE_BANNER}
+      />
+      <Spancing y={10} />
+      <Text.Subtitle className="px-[5%]">Próximos churras</Text.Subtitle>
+      <Calendar />
+    </View>
   );
 }
